0e99e614b309d2826a3ccc8e31687cf7e85fadc9
Caio Ribeiro <blueheart@tuta.io>
2019-10-26 21:51:51 -0300

    add load, and auto_complete skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod utils;
-
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -8,12 +8,64 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+
+type Alpha = Box<HashMap<char, Node>>;
+
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct Node {
+    terminal: bool,
+    alpha: Alpha
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, spell-checker!");
+pub struct Trie {
+    root: Node
+}
+
+#[wasm_bindgen]
+impl Trie {
+
+    pub fn new() -> Self {
+        Trie { root: Node {terminal: false, alpha: Box::new(HashMap::new())} }
+    }
+
+    pub fn load(&mut self, file: String) {
+        let words = file.lines();
+        for word in words {
+            self.new_word(word.trim().to_string());
+        }
+    }
+
+    //uses a depth first search to retrieve all
+    //words with the given prefix
+    pub fn auto_complete(self, prefix: String) {
+
+    }
+
+    //loads a word into Trie
+    pub fn new_word(&mut self, word: String) {
+        let mut iter = &mut self.root;
+        for c in word.chars() {
+            if iter.alpha.contains_key(&c) {
+                iter = iter.alpha.get_mut(&c).unwrap();
+            }
+            else {
+                iter.alpha.insert(c, Node {terminal: false, alpha: Box::new(HashMap::new())});
+                iter = iter.alpha.get_mut(&c).unwrap();
+            }
+        }
+        iter.terminal = true;
+    }
+
+    //search Trie for word
+    pub fn search(&mut self, word: String) -> bool {
+        let mut iter: &mut Node = &mut self.root;
+        for c in word.chars() {
+            if iter.alpha.contains_key(&c) {
+                iter = iter.alpha.get_mut(&c).expect("couldn't get node");
+            }
+            else { return false }
+        }
+        return iter.terminal
+    }
 }
